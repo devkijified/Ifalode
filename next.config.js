58240e434115538ca2c2045605e5d5b4b813e820
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-08-27 00:53:47 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['eshjeeahmjzwshyxpcns.supabase.co'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+}
+
+module.exports = nextConfig
