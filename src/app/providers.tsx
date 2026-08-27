bc0427f13d81a9ac516069e25c9e7d2e33f09381
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-08-27 01:36:02 +0100

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,5 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
-import { ThemeProvider } from 'next-themes'
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <SessionProvider>
-      <ThemeProvider attribute="class" defaultTheme="light">
-        {children}
-      </ThemeProvider>
-    </SessionProvider>
-  )
+  return <>{children}</>
 }
