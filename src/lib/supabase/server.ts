1b802f12a8c905aab34a853e0f2d46ed9b3f75b5
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-08-28 03:50:56 +0100

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,8 +2,8 @@ import { createServerClient as createSupabaseServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { Database } from './types'
 
-export const createServerClient = () => {
-  const cookieStore = cookies()
+export const createServerClient = async () => {
+  const cookieStore = await cookies()
   
   return createSupabaseServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
